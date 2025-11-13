762a9ab15bfbf74d355107bfa76e44844e0ec425
Илья Литавор <ilitavor@mail.ru>
2025-11-13 22:06:51 +0300

    Добавлен метод delDigit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Calculator(MainWindow):
         self.first_dig = True
         self.number = tk.StringVar()
         self.number.set("0")
+        self.entry1.config(textvariable=self.expression)
         self.entry2.config(textvariable=self.number)
         self.button_0.config(command=lambda: self.setNum("0"))
         self.button_1.config(command=lambda: self.setNum("1"))
@@ -21,6 +22,7 @@ class Calculator(MainWindow):
         self.button_7.config(command=lambda: self.setNum("7"))
         self.button_8.config(command=lambda: self.setNum("8"))
         self.button_9.config(command=lambda: self.setNum("9"))
+        self.button_del.config(command=self.delDigit)
 
     def setNum(self, num: chr) -> None:
         if (self.number.get() == "0") or (self.first_dig == True):
@@ -28,6 +30,11 @@ class Calculator(MainWindow):
         else:
             self.number.set(self.number.get() + num)
         self.first_dig = False
+
+    def delDigit(self) -> None:
+        if (len(self.number.get()) < 2 or ("-" in self.number.get() and len(self.number.get()) == 2)):
+            self.number.set("0")
+        else: self.number.set(self.number.get()[0:len(self.number.get())-1])
 
 if __name__ == '__main__':
     calc = Calculator()
